f92aaebec800a52a2bfc78d4446a6d5426050b23
Kadar Hall <kadarhall@Kadars-MBP.home>
2015-01-11 21:55:03 -0500

    functionality added

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,10 @@
-def Student < ActiveRecord::Base
+class Student < ActiveRecord::Base
   belongs_to :house
+
+  before_save :student_house
+
+  def student_house
+  	self.house = House.all.sample
+  end
+
 end
